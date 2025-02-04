4d3cd4470050bef66b05245de5f92da27b9137cf
Fewrick <frederikudby556@gmail.com>
2025-02-04 09:31:53 +0100

    Update uge1.py

--- Uge1/uge1.py
+++ Uge1/uge1.py
@@ -4,3 +4,14 @@ from skimage.transform import rescale, resize
 import matplotlib.pyplot as plt
 import numpy as np
 import pydicom as dicom
+
+# Directory containing data and images
+in_dir = "data/"
+
+# X-ray image
+im_name = "metacarpals.png"
+
+# Read the image.
+# Here the directory and the image name is concatenated
+# by "+" to give the full path to the image.
+im_org = io.imread(in_dir + im_name)
