bd0c39566baa51af6db8b3eae6fda6582aba6f30
Fewrick <frederikudby556@gmail.com>
2025-02-11 09:13:25 +0100

    Update uge2.py

---
+++ Uge2/ex1b-PCA/uge2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+in_dir = "ex1b-PCA/data/"
+
+txt_name = "iris_data.txt"
+
+iris_data = np.loadtxt(in_dir + txt_name, comments="%")
+# x is a matrix with 50 rows and 4 columns
+x = iris_data[0:50, 0:4]
