cf02fd06ef5a8ed8d8eb73c1832f6f7e6a096481
Fewrick <frederikudby556@gmail.com>
2025-02-11 09:15:19 +0100

    ye

--- Uge2/uge2.py
+++ Uge2/uge2.py
@@ -8,3 +8,7 @@ txt_name = "iris_data.txt"
 iris_data = np.loadtxt(in_dir + txt_name, comments="%")
 # x is a matrix with 50 rows and 4 columns
 x = iris_data[0:50, 0:4]
+
+n_feat = x.shape[1]
+n_obs = x.shape[0]
+print(f"Number of features: {n_feat} and number of observations: {n_obs}")
