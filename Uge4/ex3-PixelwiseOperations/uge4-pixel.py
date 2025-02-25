55c224a0796717d47c9fd423403b164bb51ef4ff
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2025-02-25 10:23:35 +0100

    virker

--- Uge4/ex3-PixelwiseOperations/uge4-pixel.py
+++ Uge4/ex3-PixelwiseOperations/uge4-pixel.py
@@ -1,3 +1,69 @@
+from skimage import io,util,exposure
 from skimage.util import img_as_float
 from skimage.util import img_as_ubyte
+import matplotlib.pyplot as plt
+import numpy as np
 
+from skimage.filters import threshold_otsu
+
+im_org = io.imread("data/vertebra.png")
+
+min_val = im_org.min()
+max_val = im_org.max()
+print("Min:",min_val,"Max:",max_val)
+
+img_float = img_as_float(im_org)
+min_val = img_float.min()
+max_val = img_float.max()
+print("Min:",min_val,"Max:",max_val)
+
+plt.hist(im_org.ravel(),bins=256)
+plt.title("Histogram of vertebra")
+plt.show()
+
+
+def histogram_stretch(img_in):
+    """
+    Stretches the histogram of an image 
+    :param img_in: Input image
+    :return: Image, where the histogram is stretched so the min values is 0 and the maximum value 255
+    """
+    # img_as_float will divide all pixel values with 255.0
+    img_float = img_as_float(img_in)
+    min_val = img_float.min()
+    max_val = img_float.max()
+    min_desired = 0.0
+    max_desired = 1.0
+
+
+    img_out = np.vectorize(lambda x: (x - min_val)*(max_desired - min_desired) / (max_val - min_val) + min_desired)(img_float)
+    # img_as_ubyte will multiply all pixel values with 255.0 before converting to unsigned byte
+    return img_as_ubyte(img_out)
+
+io.imshow(histogram_stretch(im_org))
+io.show()
+
+
+def gamma_map(img, gamma):
+    img_float = img_as_float(img)
+
+    return img_as_ubyte(np.vectorize(lambda x: np.power(x,gamma))(img_float))
+
+io.imshow(gamma_map(im_org,2.0))
+io.show()
+
+
+def threshold_image(img_in, thres):
+
+    apply = np.vectorize(lambda x: 255 if x > thres else 0)
+
+    return apply(img_in)
+
+io.imshow(threshold_image(im_org,140))
+io.show()
+
+
+thres = threshold_otsu(im_org)
+
+io.imshow(threshold_image(im_org,thres))
+io.show()
---
+++ Uge4/ex3-PixelwiseOperations/video_thresh.py
@@ -0,0 +1,73 @@
+import time
+import cv2
+import numpy as np
+from skimage.util import img_as_float
+from skimage.util import img_as_ubyte
+from skimage.filters import threshold_otsu
+
+
+def show_in_moved_window(win_name, img, x, y):
+    """
+    Show an image in a window, where the position of the window can be given
+    """
+    cv2.namedWindow(win_name)
+    cv2.moveWindow(win_name, x, y)
+    cv2.imshow(win_name, img)
+
+def capture_from_camera_and_show_images():
+    print("Starting image capture")
+
+    print("Opening connection to camera")
+    url = 0
+    use_droid_cam = True
+    if use_droid_cam:
+        url = "http://10.209.125.207:4747/video"
+    cap = cv2.VideoCapture(url)
+    # cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    print("Starting camera loop")
+    # Get first image
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame")
+        exit()
+
+    stop = False
+    while not stop:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame. Exiting ...")
+            break
+
+        # Transform image to gray scale and then to float, so we can do some processing
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_float = img_as_float(frame_gray)
+
+
+        thres = threshold_otsu(frame_float)
+
+
+        
+
+        threshholding = np.vectorize(lambda x: 0.0 if x < thres else 1.0)
+
+        thres_img = threshholding(frame_float)
+
+        # Display the resulting frame
+        show_in_moved_window("Input", frame, 0, 10)
+        show_in_moved_window("Difference image", thres_img, 1200, 10)
+
+        if cv2.waitKey(1) == ord("q"):
+            stop = True
+
+    print("Stopping image loop")
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    capture_from_camera_and_show_images()
